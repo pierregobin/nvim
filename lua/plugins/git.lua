4b92c60dea187657813538a336eb923f312edbe2
pierre <pierregobin@free.Fr>
2024-01-27 21:06:06 +0100

    add git plugin

---
+++ lua/plugins/git.lua
@@ -0,0 +1,11 @@
+return {
+  -- add symbols-outline
+  {
+    "tpope/vim-fugitive",
+    cmd = "Git",
+    keys = {},
+    opts = {
+      -- add your options that should be passed to the setup() function here
+    },
+  },
+}
