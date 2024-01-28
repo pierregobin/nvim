942885c4161d6aa883acc6f66331b0b907398530
pierre <pierregobin@free.Fr>
2024-01-28 10:05:46 +0100

    support of vim-fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,10 +2,5 @@ return {
   -- add symbols-outline
   {
     "tpope/vim-fugitive",
-    cmd = "Git",
-    keys = {},
-    opts = {
-      -- add your options that should be passed to the setup() function here
-    },
   },
 }
